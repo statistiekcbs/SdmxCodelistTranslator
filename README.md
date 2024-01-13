f8015cb81c80fa105529f96b1518ad112038e598
Pascal CBS <pascal.fransen.cbs@gmail.com>
2024-01-13 11:51:25 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# SdmxCodelistTranslator
-Translate SDMX artefacts tfor use in your local language
+# Sdmx Codelist Translator
+Get a SDMX codelist from a remote repository and translate it into your favourite language.
+
+# Setup VS Code
+## clone repo : 
+'''
+    git clone https://github.com/statistiekcbs/SdmxCodelistTranslator.git
+'''
+## create a virtual environment (venv)
+- in Visual Code : F1 -> Python Create Environment ...
+CLI
+'''
+python -m venv .
+'''
+
+## install packages
+CLI : 
+'''
+pip: -r requirements.txt
+'''
+VS Code : Mark requirements.txt checkbox during creation of the virtual environment
---
+++ translate.py
@@ -0,0 +1,103 @@
+import sdmx
+import sys
+import argparse
+import logging
+import translators as ts
+from datetime import datetime
+from sdmx.model.v21 import Agency
+from sdmx.message import StructureMessage, Header
+
+
+""" ---------------------------------------------------------------------------
+    Settings
+--------------------------------------------------------------------------- """
+# logging.basicConfig(level=logging.DEBUG)
+
+
+""" ---------------------------------------------------------------------------
+    Functions
+--------------------------------------------------------------------------- """
+DEFAULTLANGUAGE = 'en'
+
+def UpdateUrn(code):
+    """
+    Assign new agency to urn's
+    """
+    code.urn = code.urn.replace(code.urn_group["agency"], target_agency)
+
+
+def TranslateCode(code):
+    """
+    Translate code names to a specified language. Source language is always defaulted to be English
+    """
+    try:
+        translated_name = ts.translate_text(
+            code.name[DEFAULTLANGUAGE], to_language=target_language)
+        code.name = {DEFAULTLANGUAGE: code.name[DEFAULTLANGUAGE],
+                     target_language: translated_name}
+        logging.debug("Translated to ", translated_name)
+    except Exception as err:
+        logging.error(f"Unexpected {err=}, {type(err)=}")
+
+
+def TranslateCodeList(codelist):
+    for row in codelist.items:
+        code = codelist.items[row]
+        TranslateCode(code)
+        UpdateUrn(code)
+        logging.debug(code.id, " - ",
+                      code.name[DEFAULTLANGUAGE], " - ", code.name[target_language])
+
+
+def WriteCodeList(codelist):
+    """
+    Save the codelist to a SDMX file
+    """
+    header = Header(id=codelist.id,
+                    prepared=datetime.now(), sender=Agency(id="DarthVader"))
+
+    message = StructureMessage(codelist={codelist.id: codelist}, header=header)
+
+    with open(codelist.id + "-translated.xml", "wb") as f:
+        f.write(sdmx.to_xml(message, pretty_print=True))
+
+
+""" ---------------------------------------------------------------------------
+Main
+--------------------------------------------------------------------------- """
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--codelist', help='ID of codelist to translate')
+    parser.add_argument(
+        '--agency', help='Agency ID of the translated codelist', default="NL1")
+    parser.add_argument('--language', help='Target language', default="nl")
+
+    args = parser.parse_args()
+
+    codelist_id = args.codelist
+
+    if (codelist_id is None):
+        print("No codelist")
+        sys.exit(0)
+
+    target_agency = args.agency
+    target_language = args.language
+
+    client = sdmx.Client(
+        "IMF",
+        backend="sqlite",
+        fast_save=True,
+        expire_after=600,
+    )
+
+    try:
+        codelist = client.codelist(codelist_id).codelist[0]
+        codelist.maintainer = Agency(name=target_agency, id=target_agency)
+        UpdateUrn(codelist)
+        TranslateCode(codelist)
+        TranslateCodeList(codelist)
+        WriteCodeList(codelist)
+    except Exception as err:
+        logging.error(f"Could not translate codelist {err=}, {type(err)=}")
